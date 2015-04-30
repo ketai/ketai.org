432967c4118b9c0010a0ea728f230e00838b0e8a
jaskiratr <randj063@newschool.edu>
2015-04-30 06:19:18 -0400

    Auto commit

--- autocompile.js
+++ autocompile.js
@@ -5,7 +5,7 @@ watch.watchTree("content", { ignoreDotFiles: true } , function(f, cur, prev) {
 	exec("woods . -p 3001 -e out", function() {		// Woods uses 3001 to render pages. statik uses 3000 to host it. Cannot render and export at same time. 
 		console.log("Compiled!");
 	});
-	exec('commit.sh' , function(){
+	exec('bash commit.sh' , function(){
 		console.log("Pushed!");
 	});
 });
